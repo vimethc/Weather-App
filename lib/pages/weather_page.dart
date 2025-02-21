d3053f3f996f6e26d8296dcd7b79a26756aacd5f
Chamindu Vimeth <vimethc@gmail.com>
2025-02-21 20:30:38 +0530

    Centered city name and temperature text in WeatherPage for better UI alignment.

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +10,51 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+
+  //api key
+  final _WeatherService = WeatherService('8bb69ae44a4c4a6ee561250827959052');
+  Weather? _weather;
+  
+  //fetch weather
+  _fetchWeather() async{
+    //get the current city
+    String cityName = await _WeatherService.getCurrentCity();
+
+    //get weather for city
+    try {
+      final weather = await _WeatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  //weather animations
+
+  //init state 
+  @override
+  void initState() {
+    super.initState();
+
+    //fetch weather on startup
+    _fetchWeather();
+  }
   @override
   Widget build(BuildContext context) {
-  return Scaffold();
+    return Scaffold(
+      body: Center (
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment. center, 
+          children: [
+            // city name
+            Text (_weather?.cityName ?? "loading city.."),
+            // temperature
+            Text ('${_weather?.temperature.round()}°C')
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,57 +1,45 @@
 import 'dart:convert';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
 import '../models/weather_model.dart';
 import 'package:http/http.dart' as http;
-import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart';
 
-class WeatherServices {
-  static const String BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+class WeatherService {
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
-  WeatherServices(this.apiKey);
+  WeatherService(this.apiKey);
 
-  Future<Weather> getWeather(String cityName) async {
-    final response = await http.get(
-      Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'),
-    );
+  Future<Weather> getWeather(String CityName) async {
+    final response = await http
+    .get(Uri.parse('$BASE_URL?q=$CityName&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
-      return Weather.fromJson(jsonDecode(response.body));
+      return Weather. fromJson (jsonDecode (response.body) );
     } else {
-      throw Exception('Failed to load weather data');
+    throw Exception( 'Failed to load weather data');
     }
   }
 
   Future<String> getCurrentCity() async {
-    // Request location permission
+    // get permission from user
     LocationPermission permission = await Geolocator.checkPermission();
-    
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        throw Exception('Location permissions are denied');
-      }
+    if (permission == LocationPermission.denied){
+      permission = await Geolocator. requestPermission();
     }
 
-    if (permission == LocationPermission.deniedForever) {
-      throw Exception('Location permissions are permanently denied. Please enable them in settings.');
-    }
+  // fetch the current location
+  Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
 
-    // Fetch the current location
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
+  // convert the location into a list of placemark objects
+  List<Placemark> placemarks =
+      await placemarkFromCoordinates (position. latitude, position. longitude);
+      
+  // extract the city name from the first placemark
+  String? city = placemarks [0]. locality;
 
-    // Convert the location into a list of placemark objects
-    List<Placemark> placemarks = await placemarkFromCoordinates(
-      position.latitude, position.longitude,
-    );
-
-    if (placemarks.isNotEmpty) {
-      String? city = placemarks[0].locality;
-      return city ?? "Unknown City";
-    } else {
-      return "Unknown City";
-    }
+  return city ?? "";
   }
 }
